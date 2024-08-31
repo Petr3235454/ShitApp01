4441e8220adb8b124ba1b7594fc75ccd30471253
Petr3235454 <petrponamarev66@gmail.com>
2024-08-31 06:15:28 +0300

    small changes

--- ShitApp01/EmployeeServices/EmployeeData.cs
+++ ShitApp01/EmployeeServices/EmployeeData.cs
@@ -7,21 +7,29 @@ using System.Threading.Tasks;
 using ShitApp01.Models;
 using System.IO;
 
-//namespace ShitApp01.EmployeeServices
-//{
-//    internal class EmployeeData
-//    {
-//        public static void SaveEmployeesToJson(List<Employee> employees)
-//        {
-//            string jsonList = JsonSerializer.Serialize(employees);
-//            File.WriteAllText("employees.json", jsonList);
-//        }
-//
-//        public static List<Employee> LoadEmployeesFromJson()
-//        {
-//            string jsonList = File.ReadAllText("employees.json");
-//            return JsonSerializer.Deserialize<List<Employee>>(jsonList);
-//
-//        }
-//    }
-//}
+namespace ShitApp01.EmployeeServices
+{
+   internal class EmployeeData
+   {
+
+       void SEX() { }
+
+
+
+
+
+
+       public static void SaveEmployeesToJson(List<Employee> employees)
+       {
+           string jsonList = JsonSerializer.Serialize(employees);
+           File.WriteAllText("employees.json", jsonList);
+       }
+
+       public static List<Employee> LoadEmployeesFromJson()
+       {
+           string jsonList = File.ReadAllText("employees.json");
+           return JsonSerializer.Deserialize<List<Employee>>(jsonList);
+
+       }
+   }
+}
